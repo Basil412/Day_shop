cb3c8c9315c218edcd5e9bb0b4d0dc9caa7a3352
Basil412 <kirdankorabel11@gmail.com>
2018-06-27 10:26:34 +0300

    POST: process.env.PORT commit

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://bbbb:qwerty1234@ds119651.mlab.com:19651/basil412'
+    url: 'mongodb://bbbb:qwerty1234@ds119651.mlab.com:19651/basil412',
+    PORT: process.env.PORT || 3000
 };
